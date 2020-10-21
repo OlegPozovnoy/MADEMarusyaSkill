c43c0010557f6ca79ed734c362206e423359d662
Oleg Pozovnoy <pozovnoy.oleg@gmail.com>
2020-10-21 18:38:58 +0300

    marusya

--- app.py
+++ app.py
@@ -1,11 +1,66 @@
+from __future__ import unicode_literals
+
+import json
+import logging
+import random
+
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('/index')
 def index():
-    return '<h1> deployed</h1>'
+    return '<h1>MADE Marusya Skill</h1>'
+
+
+@app.route('/marusya', methods=['POST'])
+def postJsonHandler():
+    logging.info(request.is_json)
+    content = request.get_json()
+    command = content['request']['command'].lower().split()
+    logging.info(f'\ncommand: {command}')
+    card = {}
+    buttons = []
+    text = ''
+    image_array = [1,2]
+    if request.json["session"]["new"]:
+        text = 'Привет, я домашнее задание для MADE и пока ничего не умею'
+    elif request.json["request"]["command"] == 'on_interrupt':
+        text = 'Прощайте. Да прибудет с вами ... ох уж эти старые привычки'
+    elif "привет" in command:
+        text = "Привет!"
+    elif "картинка" in command:
+        text = "Вот вам картинка!"
+        card = {
+            "type": 'BigImage',
+            "image_id": random.choice(image_array)
+        }
+    elif "карусель" in command:
+        text = "Вот вам карусель!"
+        card = {
+            "type": 'ItemsList',
+            "image_id": image_array
+        }
+    elif "кнопки" in command:
+        text = "Вот вам кнопки: кто же GOAT в теннисе?"
+        buttons = [{"title": "Джокович"},{"title": "Надаль"},{"title": "Федерер"}]
+    else:
+        text = "Шпрехен зе дойч-хендехох! Нихт понимать!"
+
+    response = {
+        "version": request.json['version'],
+        "session": request.json['session'],
+        "response": {
+            "end_session": False,
+            "text": text,
+            "card": card,
+            "buttons": buttons
+        }
+    }
+    logging.info(f"response: {response}")
+    return json.dumps(response, ensure_ascii=False, indent=2)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
